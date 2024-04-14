e25b2438fbf43ffdb696b9e3c8c33b87c7e75b98
wildanriz <wildanrizkia47@gmail.com>
2024-04-14 23:44:47 +0700

    first commit

---
+++ resources/views/landing/index.blade.php
@@ -0,0 +1,276 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Wildan Rizkia </title>
+        <link rel="stylesheet" href="css/style.css" />
+        <!-- Animated style -->
+        <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+      />
+      <!-- Animated Scroll AOS -->
+      <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+        
+    </head>
+
+    <body>
+        <!-- Header -->
+        <header>
+            <div class="maioncontainer">
+                <div class="nav animate__animated animate__fadeInDown">
+                   <div class="logo">
+                    <a href="/">Wildan Rizkia</a>
+                   </div> 
+
+                   <nav>
+                    <ul>
+                        <li id="link1"><a href="#AboutMe">About Me</a></li>
+                        <li id="link2"><a href="#Services">Services</a></li>
+                        <li id="link3"><a href="#Portofolios">Portfolio</a></li>
+                        <li id="link4"><a href="#Skills">Skills</a></li>
+                        <li id="link5"><a href="#Organizationexperience">Organization Experience</a></li>
+
+                    </ul>
+                   </nav>
+
+                   <div class="layer">
+                    <div class="line-1"></div>
+                    <div class="line-2"></div>
+                    <div class="line-3"></div>
+                   </div>
+                </div>
+
+                <section id="hero">
+                    <div class="hero-left animate__animated animate__fadeInLeft animate__delay-2s">
+                        <img src="./images/FTwildan2.png" alt="Person">
+    
+                    </div>
+    
+                    <div class="hero-right animate__animated animate__fadeInRight animate__delay-1s">
+                        <h3 class="pre-tittle">My name is</h3>
+                        <h1 class="hero-name">Wildan Rizkia Noor Akhsal Hermawan</h1>
+                        <h1 class="hero-section-tittle">Fullstack Developers</h1>
+                        <h1 class="hero-tittle">Undergraduate Computer Science Student at BINUS University</h1>
+    
+                    </div>
+                </section>
+            </div>
+
+
+        </header>
+        <!-- End of Header -->
+        <!-- About Me -->
+        <section id="AboutMe">
+            <div class="about maincontainer">
+             <h3 class="pre-tittle" data-aos="fade-up" data-aos-duration="1000" >About Me</h3>
+             <h1 class="section-tittle" data-aos="fade-up" data-aos-duration="2000">Personal & Education</h1>
+             
+             <div class="about-grid">
+                <div class="about-left">
+                    <div class="education">
+                        <div class="line" data-aos="fade-right"data-aos-duration="1000">
+                            <div></div>
+                        </div>
+
+                        <div class="education-info " data-aos="fade-right" data-aos-duration="1000">
+                            <h4 class="education-tittle">Bina Nusantara University</h4>
+                            <p>Bachelor Degree of Computer Science</p>
+                            <h4 class="education-years ">
+                                2022 - <span>Present</span>
+                            </h4>
+                        </div>
+                    </div>
+                    <div class="education">
+                        <div class="line" data-aos="fade-right" data-aos-duration="1000">
+                            <div></div>
+                        </div>
+
+                        <div class="education-info" data-aos="fade-right" data-aos-duration="1000">
+                            <h4 class="education-tittle ">SMA Negeri 5 Malang</h4>
+                            <p>Senior High School</p>
+                            <h4 class="education-years">
+                                2016 - 2019
+                            </h4>
+                        </div>
+                    </div>
+                    <div class="education"  >
+                        <div class="line" data-aos="fade-right" data-aos-duration="1000">
+                            <div></div>
+                        </div>
+
+                        <div class="education-info" data-aos="fade-right" data-aos-duration="1000">
+                            <h4 class="education-tittle">SMP Negeri 21 Malang</h4>
+                            <p >Junior High School</p>
+                            <h4 class="education-years">
+                                2013 - 2016
+                            </h4>
+                        </div>
+                    </div>
+                </div>
+                <div class="about-right" >
+                    <h1 class="about-section-tittle" data-aos="zoom-in" data-aos-duration="1000">Hi, Welcome to My Portfolio Website!</h1>
+                    <p class=""data-aos="fade-up" data-aos-duration="3000">                    My name is Wildan Rizkia Noor Akhsal Hermawan. Currently, I am an undergraduate student of BINUS University. I am a dedicated professional in the world of graphic design and web development. With experience in this industry, I have successfully created a variety of innovative and stunning projects. From creative logo design to responsive website development. I strongly emphasize quality, sustainability and customer satisfaction in every project I take on. With great passion and dedication, I am ready to bring my creativity and expertise to help realize your vision and mission in web design and development projects.</p>
+                    <div class="about-mid" >
+                        <h1 class="about-section-tittle" data-aos="fade-left" data-aos-duration="3000">Socials</h1>
+                        <a href="" data-aos="fade-up" data-aos-duration="1000"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M15.233 5.488c-.843-.038-1.097-.046-3.233-.046s-2.389.008-3.232.046c-2.17.099-3.181 1.127-3.279 3.279-.039.844-.048 1.097-.048 3.233s.009 2.389.047 3.233c.099 2.148 1.106 3.18 3.279 3.279.843.038 1.097.047 3.233.047 2.137 0 2.39-.008 3.233-.046 2.17-.099 3.18-1.129 3.279-3.279.038-.844.046-1.097.046-3.233s-.008-2.389-.046-3.232c-.099-2.153-1.111-3.182-3.279-3.281zm-3.233 10.62c-2.269 0-4.108-1.839-4.108-4.108 0-2.269 1.84-4.108 4.108-4.108s4.108 1.839 4.108 4.108c0 2.269-1.839 4.108-4.108 4.108zm4.271-7.418c-.53 0-.96-.43-.96-.96s.43-.96.96-.96.96.43.96.96-.43.96-.96.96zm-1.604 3.31c0 1.473-1.194 2.667-2.667 2.667s-2.667-1.194-2.667-2.667c0-1.473 1.194-2.667 2.667-2.667s2.667 1.194 2.667 2.667zm4.333-12h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm.952 15.298c-.132 2.909-1.751 4.521-4.653 4.654-.854.039-1.126.048-3.299.048s-2.444-.009-3.298-.048c-2.908-.133-4.52-1.748-4.654-4.654-.039-.853-.048-1.125-.048-3.298 0-2.172.009-2.445.048-3.298.134-2.908 1.748-4.521 4.654-4.653.854-.04 1.125-.049 3.298-.049s2.445.009 3.299.048c2.908.133 4.523 1.751 4.653 4.653.039.854.048 1.127.048 3.299 0 2.173-.009 2.445-.048 3.298z"/></svg>Instagram</a>
+                        <a href="https://github.com/wildanriz" data-aos="fade-up" data-aos-duration="1500"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-4.466 19.59c-.405.078-.534-.171-.534-.384v-2.195c0-.747-.262-1.233-.55-1.481 1.782-.198 3.654-.875 3.654-3.947 0-.874-.312-1.588-.823-2.147.082-.202.356-1.016-.079-2.117 0 0-.671-.215-2.198.82-.64-.18-1.324-.267-2.004-.271-.68.003-1.364.091-2.003.269-1.528-1.035-2.2-.82-2.2-.82-.434 1.102-.16 1.915-.077 2.118-.512.56-.824 1.273-.824 2.147 0 3.064 1.867 3.751 3.645 3.954-.229.2-.436.552-.508 1.07-.457.204-1.614.557-2.328-.666 0 0-.423-.768-1.227-.825 0 0-.78-.01-.055.487 0 0 .525.246.889 1.17 0 0 .463 1.428 2.688.944v1.489c0 .211-.129.459-.528.385-3.18-1.057-5.472-4.056-5.472-7.59 0-4.419 3.582-8 8-8s8 3.581 8 8c0 3.533-2.289 6.531-5.466 7.59z"/></svg>Github</a>
+                        <a href="https://www.linkedin.com/in/wildan-rizkia-noor-akhsal-hermawan/" data-aos="fade-up" data-aos-duration="2000"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></svg> LinkedIn</a>
+                        <a href="https://www.youtube.com/@wildanhermawan839" data-aos="fade-up" data-aos-duration="2500"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M10 9.333l5.333 2.662-5.333 2.672v-5.334zm14-4.333v14c0 2.761-2.238 5-5 5h-14c-2.761 0-5-2.239-5-5v-14c0-2.761 2.239-5 5-5h14c2.762 0 5 2.239 5 5zm-4 7c-.02-4.123-.323-5.7-2.923-5.877-2.403-.164-7.754-.163-10.153 0-2.598.177-2.904 1.747-2.924 5.877.02 4.123.323 5.7 2.923 5.877 2.399.163 7.75.164 10.153 0 2.598-.177 2.904-1.747 2.924-5.877z"/></svg> Youtube</a>
+                    </div>
+                </div>
+             </div>
+            </div>
+        </section>
+        <!-- End About Me -->
+        <!-- Services -->
+        <section id="Services" >
+         <div class="services main-container" >
+            <h3 class="pre-tittle" data-aos="fade-up" data-aos-duration="1000">Services</h3>
+            <h1 class="section-tittle services-tittle" data-aos="fade-up" data-aos-duration="2000">Spesialized In</h1>
+
+            <div class="grid-3">
+              <div class="service"data-aos="fade-right" data-aos-duration="3000">
+                <h4>Web Development</h4>
+                <p >Creating and maintaining websites. It involves a range of tasks from coding and markup to the technical design and performance optimization of a website.</p>
+              </div> 
+              <div class="service" data-aos="fade-up" data-aos-duration="3000">
+                <h4 >UI/UX Design</h4>
+                <p >Creation and optimization of user interfaces (UI) for digital products, focusing on aesthetics and interactivity, while user experience (UX) design aims to enhance the overall experience of the user with the product, ensuring it is intuitive and satisfying.</p> 
+            </div>
+            <div class="service" data-aos="fade-left" data-aos-duration="3000">
+                <h4 >Application Development</h4>
+                <p>Creating software applications that involves planning, designing, coding, testing, and deploying to meet business needs. It can range from creating simple single-user applications to complex, scalable enterprise software.</p>
+         </div>   
+        </section>
+        <!-- End Services -->
+        <!-- Portofolio -->
+        <section id="Portofolios">
+            <div class="portofolios maincontainer">
+                <h3 class="pre-tittle" data-aos="fade-up" data-aos-duration="1000">My Project</h3>
+                <h1 class="section-tittle" data-aos="fade-up" data-aos-duration="2000">Featured Portfolio</h1>
+
+                <div class="grid-3">
+                    <div class="portofolio">
+                        <div class="portofolio-cover" data-aos="zoom-in" data-aos-duration="3000">
+                            <img src="./images/ARMS3.png" alt="Portofolio 1">
+                        </div>
+                        <div class="portofolio-info" data-aos="fade-up" data-aos-duration="2000">
+                            <div class="portofolio-tittle" data-aos="fade-up" data-aos-duration="2000">
+                                <h4> Augmented Reality World Monuments</h4>
+                                <a href="https://youtu.be/kk9CrDqzf3c">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"/></svg>
+                                </a>
+                            </div>
+                            <p>This AR project was created during Semester 2, using the Assemblr platform. By collecting several assets and needs about world monuments, we managed to combine all assets into one unit by prioritizing UI to user UX. The project has been completed and can be scanned using the marker-base provided by Assemblr related to the project that has been done.</p>
+                        </div>
+                    </div>
+                    <div class="portofolio">
+                        <div class="portofolio-cover" data-aos="zoom-in" data-aos-duration="3000">
+                            <img src="./images/FTwildan3.png" alt="Portofolio 2">
+                        </div>
+                        <div class="portofolio-info" data-aos="fade-up" data-aos-duration="2000">
+                            <div class="portofolio-tittle" data-aos="fade-up" data-aos-duration="2000">
+                                <h4>Augmented Reality Real Estate Application</h4>
+                                <a href="https://youtu.be/D8BJHFVr7VU"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"/></svg>
+
+                                </a>
+                            </div>
+                            <p>AR Real estate project using marker-base technology. We develop a real house that is visualized into a virtual house so that prospective buyers do not need to visit the house to be purchased.. Developing several features in it such as the building construction process, virtual tours, and others. In this development, we used several supporting software such as Blender, Unity, Adobe Photoshop.</p>
+                        </div>
+                </div>
+            </div>
+        </section>
+        <!-- End Portofolio -->
+        <!-- Skills -->
+
+        <section id="Skills">
+            <div class="skills maincontainer">
+                <h3 class="pre-tittle" data-aos="fade-up" data-aos-duration="1000">Skills</h3>
+                <h1 class="section-tittle" data-aos="fade-up" data-aos-duration="2000">About My Skills</h1>
+
+                <div class="skills-grid">
+                    <div class="skills-left">
+                        <div class="skills section-tittle " data-aos="zoom-in" data-aos-duration="2000">Hard<span>-SKILLS-</span></div>
+                        <div class="skills-list" data-aos="fade-up" data-aos-duration="2000">
+                            
+                            With more than 2 years of experience, I am very familiar with programming, such as:<span class=""data-aos="fade-up" data-aos-duration="3000" > Node JS, C, HTML, CSS, MySQL, Java Script, PHP, C#, Fr. Bootstrap, Fr. Laravel.</span>
+
+ 
+ 
+                        </div>
+                    </div>
+                    <div class="skills-right">
+                        <div class="skills section-tittle " data-aos="zoom-in" data-aos-duration="2000">Soft<span>-SKILLS-</span></div>
+                        <div class="skills-list" data-aos="fade-up" data-aos-duration="2000">
+                            I also hone my soft skills to support my performance, i have several soft skills, such as: <span class=""data-aos="fade-up" data-aos-duration="3000">Leadership, Decisiveness, Flexibility, Responsibility, Problem Solving, Teamwork, Time Management.</span>
+
+ 
+ 
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- EndSkills -->
+        <!-- Organization Ex -->
+        <section id="Organizationexperience">
+            <div class="organization maincontainer">
+                <h3 class="pre-tittle" data-aos="fade-up" data-aos-duration="1000">Organization Experience</h3>
+                <h1 class="section-tittle" data-aos="fade-up" data-aos-duration="2000">Participate in Organizations</h1>
+                
+                <div class="grid-3">
+                    <div class="organization">
+                        <div class="organization-cover " data-aos="zoom-in" data-aos-duration="3000">
+                            <img src="./images/GDSC1.jpg" alt="GDSC">
+                        </div>
+                        <div class="organization-info"data-aos="fade-up" data-aos-duration="3000">
+                            <div class="organization-tittle " data-aos="fade-up" data-aos-duration="1000">
+                                <h4>GDSC Bina Nusantara Malang (<span> September 2023 - Present </span>)</h4>
+                            
+                            </div>
+                            <div class="organization-section-tittle" data-aos="fade-up" data-aos-duration="2000">
+                                <h4>Position : 
+                                    <span>Chief Officer Lead (CO Lead) & Chief Finance Officer (CFO)</span></h4>
+                            </div>
+                            <p>Google Development Student Clubs is a learning community that is directly supervised by Google. Activities carried out such as workshops, seminars, mini-projects, many more events and of course attended by people who are experts in their fields.</p>
+                        </div>
+                    </div>
+                    <div class="organization">
+                        <div class="organization-cover" data-aos="zoom-in" data-aos-duration="3000">
+                            <img src="./images/BNCC.png" alt="GDSC">
+                        </div>
+                        <div class="organization-info" data-aos="fade-up" data-aos-duration="3000">
+                            <div class="organization-tittle" data-aos="fade-up" data-aos-duration="1000">
+                                <h4>Bina Nusantara Computer Club (<span> December 2022 - July 2023 </span>)</h4>
+                            </div>
+                            <div class="organization-section-tittle" data-aos="fade-up" data-aos-duration="2000">
+                                <h4 >Position : <span>Back End Developers - Member</span></h4>
+                            </div>
+                            <p>
+                                Bina Nusantara Computer Club is a student organization or (UKM). BNCC provides learning activities for students who want to learn more about Fullstack Developers (Front-Back End, UI/UX Designer). As a member, I participate by attending UKM activities every week and working on the projects given.</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </section>
+        <!-- End Organization Ex -->
+
+        <!-- Footers -->
+
+        <footer>
+            <p>&#169; 2024 - Made by Wildan Rizkia</p>
+        </footer>
+        <!-- End Footers -->
+
+        <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+        <script>
+          AOS.init();
+        </script>
+
+        <script src="script.js"></script>
+    </body>
+</html>
